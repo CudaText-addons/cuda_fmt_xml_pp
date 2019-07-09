f832e55a68f3154d9e1d845db4be799b972eb1d5
Alexey-t <support@uvviewsoft.com>
2019-07-09 23:52:01 +0300

    add xml.dom

--- __init__.py
+++ __init__.py
@@ -1,12 +1,32 @@
-import cudatext as app
+import re
+import xml.dom.minidom
 from . import xmlpp
+import cudatext as app
 
-WIDTH = 100 #reformat at max width
+def format_pp(text):
 
-def format(text):
+    WIDTH = 100 #reformat at max width
     indent = app.ed.get_prop(app.PROP_TAB_SIZE)
     eol = '\n'
     text = xmlpp.get_pprint(text, indent=indent, width=WIDTH)
     lines = text.splitlines()
     text = eol.join(lines)+eol
     return text
+
+def format_dom(source):
+
+    try:
+        parsedCheck = xml.dom.minidom.parseString(source)
+    except xml.parsers.expat.ExpatError as e:
+        app.msg_box('XML Tidy:\n'+str(e), app.MB_OK+app.MB_ICONERROR)
+        return
+
+    re_compile = re.compile("<.*?>")
+    result = re_compile.findall(source)
+    source = ''.join(result)
+
+    indent_text = ' '*app.ed.get_prop(app.PROP_TAB_SIZE)
+    parsed = parsedCheck
+    lines = parsed.toprettyxml(indent=indent_text).split('\n')
+    source = '\n'.join([line for line in lines if line.strip()])
+    return source
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,8 +1,11 @@
-Formatter for CudaText.
-Reformats/indents XML code.
+Formatters for XML lexer.
+
+- XML Pretty Print
 Uses xmlpp library from http://xmlpp.codeplex.com/
 which is not popular but should work good.
 
+- XML Tidy
+Uses xml.dom Python library which requires OK structure of XML file.
+
 Author: Alexey T. (CudaText)
 License: MIT
-
